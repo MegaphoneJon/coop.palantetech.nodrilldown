acb5c81725ad8c0d3df7fe128d4964da4ea3ef14
Jon Goldberg <jon@megaphonetech.com>
2024-01-15 14:41:37 -0500

    civix format 23.02.01

--- nodrilldown.php
+++ nodrilldown.php
@@ -81,7 +81,6 @@ function nodrilldown_civicrm_alterReportVar($varType, &$var, &$object) {
   }
 }
 
-
 /**
  * Implements hook_civicrm_config().
  *
@@ -89,17 +88,6 @@ function nodrilldown_civicrm_alterReportVar($varType, &$var, &$object) {
  */
 function nodrilldown_civicrm_config(&$config) {
   _nodrilldown_civix_civicrm_config($config);
-}
-
-/**
- * Implements hook_civicrm_xmlMenu().
- *
- * @param $files array(string)
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_xmlMenu
- */
-function nodrilldown_civicrm_xmlMenu(&$files) {
-  _nodrilldown_civix_civicrm_xmlMenu($files);
 }
 
 /**
@@ -112,94 +100,12 @@ function nodrilldown_civicrm_install() {
 }
 
 /**
- * Implements hook_civicrm_uninstall().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_uninstall
- */
-function nodrilldown_civicrm_uninstall() {
-  _nodrilldown_civix_civicrm_uninstall();
-}
-
-/**
  * Implements hook_civicrm_enable().
  *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_enable
  */
 function nodrilldown_civicrm_enable() {
   _nodrilldown_civix_civicrm_enable();
-}
-
-/**
- * Implements hook_civicrm_disable().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_disable
- */
-function nodrilldown_civicrm_disable() {
-  _nodrilldown_civix_civicrm_disable();
-}
-
-/**
- * Implements hook_civicrm_upgrade().
- *
- * @param $op string, the type of operation being performed; 'check' or 'enqueue'
- * @param $queue CRM_Queue_Queue, (for 'enqueue') the modifiable list of pending up upgrade tasks
- *
- * @return mixed
- *   Based on op. for 'check', returns array(boolean) (TRUE if upgrades are pending)
- *                for 'enqueue', returns void
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_upgrade
- */
-function nodrilldown_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
-  return _nodrilldown_civix_civicrm_upgrade($op, $queue);
-}
-
-/**
- * Implements hook_civicrm_managed().
- *
- * Generate a list of entities to create/deactivate/delete when this module
- * is installed, disabled, uninstalled.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_managed
- */
-function nodrilldown_civicrm_managed(&$entities) {
-  _nodrilldown_civix_civicrm_managed($entities);
-}
-
-/**
- * Implements hook_civicrm_caseTypes().
- *
- * Generate a list of case-types
- *
- * Note: This hook only runs in CiviCRM 4.4+.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_caseTypes
- */
-function nodrilldown_civicrm_caseTypes(&$caseTypes) {
-  _nodrilldown_civix_civicrm_caseTypes($caseTypes);
-}
-
-/**
- * Implements hook_civicrm_angularModules().
- *
- * Generate a list of Angular modules.
- *
- * Note: This hook only runs in CiviCRM 4.5+. It may
- * use features only available in v4.6+.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_caseTypes
- */
-function nodrilldown_civicrm_angularModules(&$angularModules) {
-  _nodrilldown_civix_civicrm_angularModules($angularModules);
-}
-
-/**
- * Implements hook_civicrm_alterSettingsFolders().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_alterSettingsFolders
- */
-function nodrilldown_civicrm_alterSettingsFolders(&$metaDataFolders = NULL) {
-  _nodrilldown_civix_civicrm_alterSettingsFolders($metaDataFolders);
 }
 
 /**
@@ -211,8 +117,5 @@ function nodrilldown_civicrm_alterSettingsFolders(&$metaDataFolders = NULL) {
  *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_preProcess
  *
-function nodrilldown_civicrm_preProcess($formName, &$form) {
-
-}
 
 */
